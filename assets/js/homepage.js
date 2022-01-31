ca89359f9250c0eca74c1edbbdc8d2ef973296cf
xJaimeGx <beanieman019@protonmail.com>
2022-01-30 21:17:30 -0600

    added js code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,14 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
 };
+
+getUserRepos("lernantino");
